5584f4ecc608e0bd656b3aa82968e2f69e88066f
Aloysius <aloysius.tan00@gmail.com>
2026-02-20 21:44:21 +0800

    Setup Commit

---
+++ src/components/BurgerStack/BurgerStack.jsx
@@ -0,0 +1,7 @@
+// src/components/BurgerStack/BurgerStack.jsx
+
+const BurgerStack = (props) => {
+  return <ul>// map through props.ingredients</ul>;
+};
+
+export default BurgerStack;
---
+++ src/components/IngredientList/IngredientList.jsx
@@ -0,0 +1,7 @@
+// src/components/IngredientList/IngredientList.jsx
+
+const IngredientList = (props) => {
+  return <ul>// map through props.ingredients</ul>;
+};
+
+export default IngredientList;
